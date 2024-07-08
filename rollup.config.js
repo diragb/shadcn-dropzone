f3d92f158b719db24e88cb642288ca45789c4b85
Dirag Biswas <hey@diragb.dev>
2024-07-09 01:22:23 +0530

    fix: lucide-icons tree shaking

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 export default {
@@ -20,11 +22,14 @@ export default {
     },
   ],
   plugins: [
+    peerDepsExternal(),
     resolve(),
     commonjs(),
     typescript({
       tsconfig: './tsconfig.json',
+      clean: true,
     }),
+    terser(),
   ],
-  external: ['react', 'react-dom'],
+  external: ['react', 'react-dom', 'lucide-react'],
 };
