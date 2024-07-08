97d3b956dcd847a018fb4c11ec5e575892069e40
Dirag Biswas <hey@diragb.dev>
2024-07-09 01:27:24 +0530

    feat: push `/dist` temporarily to test out github pages

---
+++ dist/index.d.ts
@@ -0,0 +1,11 @@
+import React from 'react';
+import { type DropzoneProps as _DropzoneProps, type DropzoneState as _DropzoneState } from 'react-dropzone';
+export interface DropzoneState extends _DropzoneState {
+}
+export interface DropzoneProps extends Omit<_DropzoneProps, 'children'> {
+    className?: string;
+    children?: (dropzone: DropzoneState) => React.ReactNode;
+}
+import '@/styles/globals.css';
+declare const Dropzone: ({ className, children, ...props }: DropzoneProps) => React.JSX.Element;
+export default Dropzone;
